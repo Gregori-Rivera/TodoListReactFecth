37394c3486a7e3d2f1ddbce590616c23417ca9bf
Gregori-Rivera <gregori.rivera@gmail.com>
2022-09-21 22:27:08 +0000

    commit inicial

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ListGroup, Placeholder } from "react-bootstrap";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("")
+	const [tasks, setTask] = useState([])
+	
+	function handleAdd () {
+		setTask ([inputValue,...tasks])
+		setInputValue ("")
+	}
+
+	function handleDelete (index) {
+		let deleteTask = [...tasks]
+		deleteTask.splice (index,1)
+		setTask (deleteTask)
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>To Do List</h1>
+			<div className="text-center">
+				<input type="text" placeholder='New task' value={inputValue} onChange={(event)=>setInputValue(event.target.value)} />
+				<button onClick={handleAdd}>Add</button>
+			</div>
+			<h1 className= {tasks.length == 0 ? "": "vacio"}>No hay tarea</h1>
+			<ListGroup>
+				{
+					tasks.map((task,index) => {
+						return (
+							<ListGroup.Item key={index} >
+									{task}
+									<button onClick={()=>handleDelete (index)}> x </button>
+							</ListGroup.Item>
+						)
+					})
+				}
+			</ListGroup>
 		</div>
 	);
 };
